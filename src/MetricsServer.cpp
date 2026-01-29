6d836898b2e891a579037735cfd9398ac95108c3
Beatriz Antunes <157928671+Beatriz-Ceschin-Antunes@users.noreply.github.com>
2026-01-29 18:43:22 +0100

    feat: create metrics server

---
+++ src/MetricsServer.cpp
@@ -0,0 +1,27 @@
+#include <QTcpServer>
+#include <QTcpSocket>
+#include "Metrics.h"
+
+class MetricsServer : public QObject {
+    Q_OBJECT
+public:
+    MetricsServer(QObject* parent = nullptr) : QObject(parent) {
+        connect(&server, &QTcpServer::newConnection, this, [this]() {
+            auto* sock = server.nextPendingConnection();
+            connect(sock, &QTcpSocket::readyRead, sock, [sock]() {
+                const QByteArray body = Metrics::instance().toPrometheusText().toUtf8();
+                QByteArray resp =
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/plain\r\n"
+                        "Content-Length: " + QByteArray::number(body.size()) + "\r\n\r\n";
+                resp += body;
+                sock->write(resp);
+                sock->disconnectFromHost();
+            });
+        });
+        server.listen(QHostAddress::LocalHost, 9100);
+    }
+
+private:
+    QTcpServer server;
+};
