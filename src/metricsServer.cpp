1bbe5824c6003342c2ac032a4a6a7bfb32cc999b
Beatriz Antunes <157928671+Beatriz-Ceschin-Antunes@users.noreply.github.com>
2026-02-01 16:37:29 +0100

    style: clean whitespace

--- src/metricsServer.cpp
+++ src/metricsServer.cpp
@@ -3,22 +3,20 @@
 #include "Metrics.h"
 #include <MetricsServer.h>
 
-MetricsServer::MetricsServer(quint16 port, QObject* parent)
-    : QObject(parent),
-      m_server(new QTcpServer(this))
+MetricsServer::MetricsServer(quint16 port, QObject* parent): QObject(parent),m_server(new QTcpServer(this))
 {
-        connect(m_server, &QTcpServer::newConnection, this, [this]() {
-            auto* sock = m_server->nextPendingConnection();
-            connect(sock, &QTcpSocket::readyRead, sock, [sock]() {
-                const QByteArray body = Metrics::instance().toPrometheusText().toUtf8();
-                QByteArray resp =
-                        "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/plain\r\n"
-                        "Content-Length: " + QByteArray::number(body.size()) + "\r\n\r\n";
-                resp += body;
-                sock->write(resp);
-                sock->disconnectFromHost();
-            });
+    connect(m_server, &QTcpServer::newConnection, this, [this]() {
+        auto* sock = m_server->nextPendingConnection();
+        connect(sock, &QTcpSocket::readyRead, sock, [sock]() {
+            const QByteArray body = Metrics::instance().toPrometheusText().toUtf8();
+            QByteArray resp =
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/plain\r\n"
+                    "Content-Length: " + QByteArray::number(body.size()) + "\r\n\r\n";
+            resp += body;
+            sock->write(resp);
+            sock->disconnectFromHost();
         });
-        m_server->listen(QHostAddress::LocalHost, 9100);
+    });
+    m_server->listen(QHostAddress::LocalHost, 9100);
 };
